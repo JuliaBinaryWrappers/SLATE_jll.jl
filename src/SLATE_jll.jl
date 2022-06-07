6102fb62265adc3ae52a7a0c3a7cba83b995c395
jlbuild <juliabuildbot@gmail.com>
2022-06-07 20:39:15 +0000

    SLATE_jll build 2022.5.0+0

---
+++ src/SLATE_jll.jl
@@ -0,0 +1,9 @@
+# Use baremodule to shave off a few KB from the serialized `.ji` file
+baremodule SLATE_jll
+using Base
+using Base: UUID
+import JLLWrappers
+
+JLLWrappers.@generate_main_file_header("SLATE")
+JLLWrappers.@generate_main_file("SLATE", UUID("cb01ff19-587c-516c-bcf9-532d49da298d"))
+end  # module SLATE_jll
